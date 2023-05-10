4864777551a4c83f92f8c05268865ca86c0d9ce2
Kazi Islam <kazichaska@gmail.com>
2023-05-09 19:01:29 -0500

    added global store and updated others files

---
+++ client/build/precache-manifest.30498617893363be3b93d5fd324f068e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3881afd6a0482bb84731b0ff240144b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0208652fb4a964d9fd71",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "125b6c61f4e6052faef9",
+    "url": "/static/js/2.919c40e2.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.919c40e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0208652fb4a964d9fd71",
+    "url": "/static/js/main.ab2094d8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ab2094d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
